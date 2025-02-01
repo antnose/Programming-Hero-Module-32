7ea97904d713586a74662aa787fd076e3a8e171c
antnose <antnose28@gmail.com>
2025-02-01 14:31:47 +0600

    complete 2nd video of 32 module

--- Module32.js
+++ Module32.js
@@ -23,7 +23,61 @@
 // console.log(p, q);
 
 // q.job = "frontend developer";
-
 // console.log(p, q);
 
-// 8.27
+//
+// 32-2 Null Vs Undefined, Different Ways You Will get Undefined
+/**
+ * 8 Ways to get undefine
+ *
+ * 1. Variable that is not initialized will give undefined
+ * 2. Function with no return
+ * 3. parameter that is not passed will be undefined
+ * 4. if return has nothing on the right side will return undefined
+ * 5. property that doesn't exists on an object will give you undefined
+ * 6. accessing array elements outside of the index range will give you undefined
+ * 7. deleting an element inside an array
+ * 8. set a value directly to undefined
+ */
+
+// let first;
+// console.log(first);
+
+// function second(a, b) {
+//   const total = a + b;
+// }
+// console.log(second(1, 2));
+
+// function third(a, b, c, d) {
+//   const total = a + b + c + d;
+//   console.log(a, b, c, d);
+// }
+// third(12, 5);
+
+// function noNegative(a, b) {
+//   if (a < 0 || b < 0) {
+//     return;
+//   }
+//   return a + b;
+// }
+// console.log(noNegative(-2, 5));
+
+// const fifth = { id: 2, name: "ponchom", age: 40 };
+// console.log(fifth.age, fifth.salary);
+
+// const sixth = [2, 4, 56, 89, 39];
+// console.log(sixth[1], sixth[7]);
+
+// const seventh = [2, 4, 56, 89, 39];
+// you should not do it. Instead use splice
+// delete seventh[1];
+// console.log(seventh);
+
+// const eighth = undefined;
+
+// const ninth = null;
+
+// const data = { results: [], updated: null };
+
+// console.log(typeof undefined);
+// console.log(typeof null);
