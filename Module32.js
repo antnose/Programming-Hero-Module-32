1afd294633324d12990be199fdd00ec10af3c0dd
antnose <antnose28@gmail.com>
2025-02-01 18:13:10 +0600

    complete 7th video of 32 module

--- Module32.js
+++ Module32.js
@@ -177,3 +177,35 @@
 // console.log(firstServer());
 // console.log(firstServer());
 // console.log(firstServer());
+
+// function greeting(greetingHandler, name) {
+//   greetingHandler(name);
+// }
+// const name = "Rony";
+// greeting(name);
+
+// const laptop = { price: 23000, brand: "lenevo", memory: "8gb" };
+// greeting(laptop);
+
+// function greetingHandler(name) {
+//   console.log("Good Morning", name);
+// }
+
+// function greetEvening(name) {
+//   console.log("Good Evening", name);
+// }
+
+// function greetNight(name) {
+//   console.log("Good Night", name);
+// }
+
+// greeting(greetingHandler, "Hanks");
+// greeting(greetingHandler, "Tom Brady");
+// greeting(greetingHandler, "Tom Cruise");
+
+// greeting(greetEvening, "Tom Solaiman");
+// greeting(greetEvening, "Tom Salman");
+
+// greeting(greetNight, "Rasel");
+
+// document.getElementById()
