a913d984a200f3cc93658b9ce5927056cd273fb7
antnose <antnose28@gmail.com>
2025-02-01 14:45:51 +0600

    complete 3rd video of 32 module

--- Module32.js
+++ Module32.js
@@ -81,3 +81,35 @@
 
 // console.log(typeof undefined);
 // console.log(typeof null);
+
+// 32-3 Different Truthy and Falsy Values in JS
+/**
+ * Truthy:
+ * 1. true
+ * 2. any number (+ve, -ve) will be truthy other than 0
+ * 3. any string other than empty string
+ * 4. '0', 'false'
+ * 5. {} empty object
+ * 6. [] empty array
+ *
+ * Falsy:
+ * 1. false
+ * 2. 0
+ * 3. '' empty string
+ * 4. undefined
+ * 5. null
+ */
+
+// const x = false;
+// if (x) {
+//   console.log(`value of x is truthy`);
+// } else {
+//   console.log(`value of x is falsy`);
+// }
+
+// optional
+// check falsy
+// const x = 0;
+// if (!x) {
+//   console.log("Nothing");
+// }
