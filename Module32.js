65d22eaade4f2ad0133a16e81e5527fea8b5e84b
antnose <antnose28@gmail.com>
2025-02-01 13:16:25 +0600

    Complete 1st video of 32 module

---
+++ Module32.js
@@ -0,0 +1,29 @@
+// 32-1 Module Overview, Primitive And Non-Primitive Data Types
+
+// Primitive DataType
+// const a = 5;
+// const b = "Samsu kopai na ekhon ar";
+// const d = true;
+
+// Non-Primitive DataType
+// const ages = [45, 23, 67];
+// const student = { id: 23, class: 8 };
+// console.log(typeof a, typeof b, typeof ages, typeof student);
+
+// let x = 5;
+// let y = x;
+// console.log(x, y);
+
+// y = 7;
+// console.log(x, y);
+
+// let p = { job: "web developer" };
+// let q = p;
+// q = { job: "Backend end" };
+// console.log(p, q);
+
+// q.job = "frontend developer";
+
+// console.log(p, q);
+
+// 8.27
